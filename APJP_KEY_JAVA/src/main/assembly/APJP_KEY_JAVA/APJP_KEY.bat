8d68d7356b7182bab8929d96c5f791ad5c4284e0
gkmuse <goknmuse@gmail.com>
2012-07-13 17:22:59 +0800

    128bit

--- APJP_KEY_JAVA/src/main/assembly/APJP_KEY_JAVA/APJP_KEY.bat
+++ APJP_KEY_JAVA/src/main/assembly/APJP_KEY_JAVA/APJP_KEY.bat
@@ -2,11 +2,11 @@
 set a=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
 setlocal EnableDelayedExpansion
 set count=0
-:: The default length of APJP_KEY is 16
+:: The maximum length of APJP_KEY is 16
 set salt=0
-:number
-set /p salt=Enter a salt value(Integer ^<=16): 
-if !salt! gtr 16 goto number
+:::number
+::set /p salt=Enter a salt value(Integer ^<=16): 
+::if !salt! gtr 16 goto number
 set /a length=15 + !salt!
 :loop
 set /a count+=1
@@ -15,14 +15,14 @@ set buffer=!buffer!!a:~%rand%,1!
 if !count! leq !length! goto loop
 echo APJP_KEY = !buffer!
 set /a length+=1
-echo The length of APJP_KEY is !length!
+echo The maximum RC4 key length is 128 bits
 set /p question=Do you want to save APJP_KEY into APJP_KEY.txt? [Y/n] 
 if /i "!question!" == "n" goto eof
 if /i "!question!" == "N" goto eof
 if /i "!question!" == "no" goto eof
 if /i "!question!" == "No" goto eof
 if /i "!question!" == "NO" goto eof
-echo # The length of APJP_KEY is !length! > APJP_KEY.txt
-echo APJP_KEY = !buffer! >> APJP_KEY.txt
+::echo # The length of APJP_KEY is !length! > APJP_KEY.txt
+echo APJP_KEY=!buffer! >> APJP_KEY.txt
 :eof
 endlocal
--- APJP_KEY_JAVA/src/main/assembly/APJP_KEY_JAVA/APJP_KEY.py
+++ APJP_KEY_JAVA/src/main/assembly/APJP_KEY_JAVA/APJP_KEY.py
@@ -5,25 +5,27 @@ from random import random
 
 a = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 buffer = ''
+#The maximum length of APJP_KEY is 16.
+length = 16
 
-while True:
-    try:
-        salt = input("Input a salt value(Integer <= 16): ")
-        if int(salt) <= 16:
-            length = 16 + int(salt)
-            break
-    except NameError:
-        print("Your input is String. Please input Number.")
-    except SyntaxError:
-        length = 16
-        break
+#while True:
+    #try:
+        #salt = input("Input a salt value(Integer <= 16): ")
+        #if int(salt) <= 16:
+            #length = 16 + int(salt)
+            #break
+    #except NameError:
+        #print("Your input is String. Please input Number.")
+    #except SyntaxError:
+        #length = 16
+        #break
 
 for i in range(0, length):
     buffer += a[int(random()*len(a))]
 
-output = 'APJP_KEY = ' + buffer
+output = 'APJP_KEY=' + buffer
 print output
-print "The length of APJP_KEY is %s" % length
+print "The maximum RC4 key length is 128 bits."
 
 question = raw_input("Do you want to save APJP_KEY into APJP_KEY.txt? [Y/n] ")
 if question == 'n':
@@ -37,4 +39,4 @@ elif question == 'No':
 elif question == 'NO':
     SystemExit
 else:
-    open('APJP_KEY.txt', 'w').write("# The length of APJP_KEY is %s\n" % length + output)
+    open('APJP_KEY.txt', 'w').write(output)
