e177d85ee60b83ce24b85734cd280fa4695e604e
gkmuse <goknmuse@gmail.com>
2012-08-13 18:02:48 +0800

    fix APJP_KEY.bat newline bug

--- APJP_KEY_JAVA/src/main/assembly/APJP_KEY_JAVA/APJP_KEY.bat
+++ APJP_KEY_JAVA/src/main/assembly/APJP_KEY_JAVA/APJP_KEY.bat
@@ -13,9 +13,10 @@ set /a count+=1
 set /a rand=%Random%%%61
 set buffer=!buffer!!a:~%rand%,1!
 if !count! leq !length! goto loop
-echo APJP_KEY = !buffer!
-set /a length+=1
-echo The maximum RC4 key length is 128 bits
+echo APJP_KEY=!buffer!
+<nul set /p ".=!buffer!" | clip
+echo APJP_KEY had been saved in clipboard^^!
+echo The maximum RC4 key length is 128 bits.
 set /p question=Do you want to save APJP_KEY into APJP_KEY.txt? [Y/n] 
 if /i "!question!" == "n" goto eof
 if /i "!question!" == "N" goto eof
@@ -23,6 +24,7 @@ if /i "!question!" == "no" goto eof
 if /i "!question!" == "No" goto eof
 if /i "!question!" == "NO" goto eof
 ::echo # The length of APJP_KEY is !length! > APJP_KEY.txt
-echo APJP_KEY=!buffer! >> APJP_KEY.txt
+<nul set /p ".=APJP_KEY=!buffer!" >> APJP_KEY.txt
+echo.>> APJP_KEY.txt
 :eof
 endlocal
